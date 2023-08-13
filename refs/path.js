45ba9592b985235b722a8c6d24798f4261eebb02
Sergey Skorykh <kknd3390@mail.ru>
2023-08-13 21:17:06 +0700

    added editing notes

--- refs/path.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename).slice(1))
-console.log(path.parse(__filename))
-console.log(path.resolve(__dirname, '..', 'index.js')) // пытается найти определенный файл
-console.log(path.join(__dirname, '..', 'index.js')) // конкатенирует строчки
